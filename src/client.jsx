b8c2b88819c3ef421ac673ad6b537bb26456774d
Sergey Aleksandrov <sergey.aleksandrov@artworktool.com>
2016-08-08 18:14:57 +0300

    add react-bsod

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,25 @@
 import {render} from 'react-dom';
 import React, {Component} from 'react';
-import App from './routes';
+var isDevelopment = process.env.NODE_ENV === 'development' ? true : false;
 
 require('./styles/stylesheets/_include.less');
 
-render(App(), document.getElementById('root'));
+var root = document.getElementById('root');
+
+var App;
+
+if (isDevelopment) {
+    var BSOD = require('react-bsod');
+
+    try {
+        App = require('./routes').default;
+
+        render(App(), root);
+    } catch (e) {
+        render(<BSOD error={e} />, root);
+    }
+} else {
+    App = require('./routes').default;
+
+    render(App(), root);
+}
